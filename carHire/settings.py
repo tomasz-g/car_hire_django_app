bdfc4ea980afe4ce2e4fe4d08d0bc7b1e05683b8
carhire <carhiretemp@gmail.com>
2017-10-25 20:53:03 +0000

    Change path in TEMPLATES for templates/registration - html files

--- carHire/settings.py
+++ carHire/settings.py
@@ -55,7 +55,9 @@ ROOT_URLCONF = 'carHire.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['./templates',],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
