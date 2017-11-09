b6f68f2daa1cbccdc576a96358bae4173f35f248
tomasz-g <tomaszdamiang@gmail.com>
2017-11-09 01:36:50 +0100

    Move content to new javascript file (js/javascripts.js) and delete old file (js/menu.js)

--- cars_fleet/static/js/menu.js
+++
@@ -1,8 +0,0 @@
-function showHideMenu() {
-    var navigationBar = document.getElementById("nav-icon");
-    if (navigationBar.className === "top-nav") {
-        navigationBar.className = "hide";
-    } else {
-        navigationBar.className = "top-nav";
-    }
-}
