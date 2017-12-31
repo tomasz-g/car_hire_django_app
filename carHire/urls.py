765b42f5332abbaa72e5bb5ab71be5c70a434e53
tomasz-g <tomaszdamiang@gmail.com>
2017-12-31 12:42:38 +0100

    Delete unused imports

--- carHire/urls.py
+++ carHire/urls.py
@@ -16,9 +16,6 @@ Including another URLconf
 from django.conf.urls import url, include
 from django.contrib import admin
 
-from django.conf import settings
-from django.conf.urls.static import static
-
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
