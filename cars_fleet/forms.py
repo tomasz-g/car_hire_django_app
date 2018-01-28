ab30e5f02a62d2858e0621a7c38d9d958b37a006
tomasz-g <tomaszdamiang@gmail.com>
2018-01-28 17:28:10 +0100

    Add SignUpForm class and imports for cars_fleet/sign.html

--- cars_fleet/forms.py
+++ cars_fleet/forms.py
@@ -1,7 +1,23 @@
 from django import forms
 
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 
+
+# Form cars_fleet/contact.html
 class ContactForm(forms.Form):
     subject = forms.CharField(required=False, max_length=200)
     contact_email = forms.EmailField(max_length=200, label="Your email")
     message = forms.CharField(widget=forms.Textarea)
+
+
+# Form cars_fleet/signup.html
+class SignUpForm(UserCreationForm):
+    email = forms.EmailField(
+        max_length=254,
+        help_text="Required valid email address."
+    )
+
+    class Meta:
+        model = User
+        fields = ('username', 'email','password1', 'password2', )
