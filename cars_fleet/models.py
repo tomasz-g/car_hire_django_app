b3b601c5c33e00ef8ecca341af2abc754d4ec24b
tomasz-g <tomaszdamiang@gmail.com>
2018-02-23 22:23:58 +0100

    Change dates modul (datetime -> django.utils.timezone) Refactor code

--- cars_fleet/models.py
+++ cars_fleet/models.py
@@ -1,7 +1,8 @@
+from django.utils import timezone
+
 from django.db import models
 
 from django.urls import reverse
-from datetime import date
 from django.contrib.auth.models import User
 from django.core.exceptions import ValidationError
 
@@ -26,11 +27,17 @@ class CarModel(models.Model):
         )
 
     def __str__(self):
-        return '{} {}'.format(self.manufacturer_name_and_logo, self.manufacturer_model_name)
+        return '{} {}'.format(
+            self.manufacturer_name_and_logo,
+            self.manufacturer_model_name
+        )
 
 
 class CarManufacturerAndLogo(models.Model):
-    manufacturer_logo = models.ImageField(upload_to='images', verbose_name="Car Logo")
+    manufacturer_logo = models.ImageField(
+        upload_to='images',
+        verbose_name="Car Logo"
+    )
     manufacturer_logo_name = models.CharField(
         max_length=15,
         verbose_name="Make",
@@ -48,10 +55,18 @@ class CarManufacturerAndLogo(models.Model):
 
 
 class Car(models.Model):
-    car_make_and_model = models.ForeignKey(CarModel, verbose_name="Make and Model")
+    car_make_and_model = models.ForeignKey(
+        CarModel,
+        verbose_name="Make and Model"
+    )
     car_body = models.ForeignKey('BodyType')
     car_type = models.ForeignKey('CarType')
-    num_of_doors = models.DecimalField(max_digits=1, decimal_places=0, default=4, verbose_name="Doors")
+    num_of_doors = models.DecimalField(
+        max_digits=1,
+        decimal_places=0,
+        default=4,
+        verbose_name="Doors"
+    )
     passangers = models.DecimalField(max_digits=1, decimal_places=0, default=5)
     car_image = models.ImageField(upload_to='images')
 
@@ -68,7 +83,11 @@ class Car(models.Model):
         )
 
     def __str__(self):
-            return '{} {} {} Doors'.format(self.car_make_and_model, self.car_body, self.num_of_doors)
+            return '{} {} {} Doors'.format(
+                self.car_make_and_model,
+                self.car_body,
+                self.num_of_doors
+            )
 
     def get_car_body(self):
         return self.car_body.get_body_type_display()
@@ -149,7 +168,10 @@ class CarType(models.Model):
 
 
 class CarSpecs(models.Model):
-    specification = models.CharField(max_length=25, help_text="Enter a car specs (e.g. Audio System, Sunroof, Airbags)")
+    specification = models.CharField(
+        max_length=25,
+        help_text="Enter a car specs (e.g. Audio System, Sunroof, Airbags)"
+    )
 
     class Meta:
         unique_together = (('specification'),)
@@ -159,7 +181,11 @@ class CarSpecs(models.Model):
 
 
 class CarEngineSize(models.Model):
-    engine_size = models.DecimalField(max_digits=2, decimal_places=1, default=1.0)
+    engine_size = models.DecimalField(
+        max_digits=2,
+        decimal_places=1,
+        default=1.0
+    )
 
     class Meta:
         unique_together = (('engine_size'),)
@@ -215,7 +241,10 @@ class CarTransmissionType(models.Model):
 
 
 class CarRegistrationNumber(models.Model):
-    registration = models.CharField(help_text="Car Registration Number", max_length=15, unique=True)
+    registration = models.CharField(
+        help_text="Car Registration Number",
+        max_length=15, unique=True
+    )
 
     class Meta:
         unique_together = (('registration'),)
@@ -233,15 +262,32 @@ class CarInstance(models.Model):
         ('r', 'Reserved'),
     )
 
-    car_status = models.CharField(max_length=1, choices=RENT_STATUS, default='a', help_text="Car availability", verbose_name="Status")
+    car_status = models.CharField(
+        max_length=1,
+        choices=RENT_STATUS,
+        default='a',
+        help_text="Car availability",
+        verbose_name="Status"
+    )
     car_registration = models.OneToOneField(CarRegistrationNumber)
     car = models.ForeignKey(Car)
     engine_size = models.ForeignKey(CarEngineSize, null=True, blank=True)
     fuel = models.ForeignKey(CarFuelType)
     transmission = models.ForeignKey(CarTransmissionType)
     car_specifications = models.ManyToManyField(CarSpecs, verbose_name="Specs")
-    rented_to_client = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True, verbose_name="Customer Name")
-    date_of_return = models.DateField(null=True, blank=True)
+    rented_to_client = models.ForeignKey(
+        User,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        verbose_name="Customer Name"
+    )
+    date_of_rent = models.DateTimeField(
+        null=True, blank=True, verbose_name="Pick-up"
+    )
+    date_of_return = models.DateTimeField(
+        null=True, blank=True, verbose_name="Drop-off"
+    )
 
     class Meta:
         ordering = [
@@ -271,10 +317,10 @@ class CarInstance(models.Model):
     display_specs.short_description = 'Specifications'
 
     def is_overdue(self):
-        if self.date_of_return and date.today() > self.date_of_return:
+        if self.date_of_return and timezone.now() > self.date_of_return:
             return True
         return False
 
     def clean(self):
         if self.rented_to_client and self.car_status == 'a':
-            raise ValidationError("Status can't be 'Available' on rented car!")
+            raise ValidationError("Change car status!")
